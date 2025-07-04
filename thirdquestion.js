afdb625ba3f6cc86f24fcb3ebc79c7536127dfd4
mohammedfathaah <mohammedfathaah@gmail.com>
2025-07-04 14:59:38 +0530

    third question completed

---
+++ thirdquestion.js
@@ -0,0 +1,5 @@
+function adultorminor(string,age){
+    return result=age>18 ? string+"is an adult" : string+"is a minor";
+}
+console.log(adultorminor("Fathaah ",22))
+console.log(adultorminor("Fathaah",17))
