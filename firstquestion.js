ae716dc127ed184e680cea1698a607f59be29b8a
mohammedfathaah <mohammedfathaah@gmail.com>
2025-07-04 14:42:25 +0530

    firstquestion and secondquestion completed

---
+++ firstquestion.js
@@ -0,0 +1,6 @@
+function evenorodd(num1,num2){
+    const sum=(num1+num2)
+    return sum%2==0 ? "even" :"odd";
+}
+console.log(evenorodd(5,7))
+console.log(evenorodd(5,4))
---
+++ secondquestion.js
@@ -0,0 +1,4 @@
+function stringabc(number,string){
+return number>10 ? string.toUpperCase() : string.toLowerCase();
+}
+console.log(stringabc(15,"hello"))
